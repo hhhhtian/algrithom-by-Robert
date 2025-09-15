9f3e060823d42a659d4c0d01b8ed7648d79081c5
hannahtian <38304610+hhhhtian@users.noreply.github.com>
2025-09-15 11:43:49 -0500

    Update iterative_dfs.cpp

--- Undirected Graphs/iterative_dfs.cpp
+++ Undirected Graphs/iterative_dfs.cpp
@@ -1,40 +1,41 @@
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    // Input：Undirected graph + start node
+    // Output： DFS path
+    vector<int> dfsIterative(int start, unordered_map<int, vector<int>>& graph) {
+        vector<int> path;
+        unordered_set<int> visited;
+        stack<int> st;
 
-vector<int> dfsIterative(int start, unordered_map<int, vector<int>>& graph) {
-    vector<int> path;
-    unordered_set<int> visited;
-    stack<int> st;
+        st.push(start);
+        visited.insert(start);
 
-    st.push(start);
-    visited.insert(start);
+        while (!st.empty()) {
+            int node = st.top();
+            st.pop();
+            path.push_back(node);
 
-    while (!st.empty()) {
-        int node = st.top();
-        st.pop();
-        path.push_back(node);
-
-        for (auto& nei : graph[node]) {
-            if (visited.count(nei)) continue;
-            st.push(nei);
-            visited.insert(nei);
+            // transverse neighbors of the above node
+            for (auto& nei : graph[node]) {
+                if (visited.count(nei)) continue;
+                visited.insert(nei);
+                st.push(nei);
+            }
         }
+        return path;
     }
-
-    return path;
-}
+};
 
 int main() {
     unordered_map<int, vector<int>> graph;
-
-    // build a undirected graph
     graph[0] = {1, 2};
     graph[1] = {0, 3, 4};
     graph[2] = {0};
     graph[3] = {1};
     graph[4] = {1};
 
-    vector<int> res = dfsIterative(0, graph);
+    Solution sol;
+    vector<int> res = sol.dfsIterative(0, graph);
 
     cout << "DFS path: ";
     for (int x : res) cout << x << " ";
@@ -42,3 +43,6 @@ int main() {
 
     return 0;
 }
+
+
+
